5b27b61e21b43afd372e66e17a426626876f2c6f
jbosegre <jbose@mail.greenriver.edu>
2018-01-19 15:59:18 -0500

    First commit by adding the index home and style page

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: humme_000
+ * Date: 1/19/2018
+ * Time: 12:49 PM
+ */
+
+ini_set ("display_errors", 1);
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//global view directory
+$template =  new Template();
+
+//define a default route
+$f3->route('GET /', function()
+{
+    $view =  new View;
+    echo $view->render
+    ('pages/home.html');
+}
+);
