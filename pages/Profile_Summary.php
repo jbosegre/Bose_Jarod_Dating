66caa0d07781b6bf2874ed634cb3218286d1bb39
jbosegre <jbose@mail.greenriver.edu>
2018-02-02 17:24:21 -0500

    Added Personal Information content and styling

--- pages/Profile_Summary.php
+++ pages/Profile_Summary.php
@@ -38,24 +38,7 @@ error_reporting(E_ALL);
 <div class="container">
     <figure id="border">
         <figcaption>
-            <h1 class="ml-5"><br><strong>My Dating Website</strong></h1>
-            <p id="summary">Welcome to the web's most successful dating website. <strong>At My Dating</strong>
-                Website you'll meet another like minded individuals. We have the
-                highest success rate of couples on the web. User's are matched by
-                interest and location. Find out why so many others have found love on
-                our site<br><br>
-            </p>
-            <h4 class="ml-5"><strong>Hear what our users are saying about us.</strong></h4>
-            <p id="review">"I met the love of my life after only a month" - Andrea<br><br>
-                "It was so easy to set up and profile and start meeting people.
-                I didn't realize how many others were looking for love in my
-                area." -John Smith<br><br>
-                "Just try it! You'll never be the same!" - Sarah</p><br><br>
-            <button class="btn btn-primary ml-5">Create a Profile!</button>
         </figcaption>
-        <img id="datePic" class="img-fluid rounded"
-             src="http://jbose.greenriverdev.com/IT328/Bose_Jarod_Dating/images/best-dating-website.jpg"
-             alt="Dating Picture">
     </figure>
 </div>
 <script src="js/jquery.slim.min.js"></script>
