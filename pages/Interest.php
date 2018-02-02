cd62b7f2680982b55175438c8cd7cbb8d1b79422
jbosegre <jbose@mail.greenriver.edu>
2018-02-02 14:53:15 -0500

    Added all of the necessary files for the assignment

---
+++ pages/Interest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: humme_000
+ * Date: 2/2/2018
+ * Time: 11:44 AM
+ */
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css"
+          href="http://jbose.greenriverdev.com/IT328/Bose_Jarod_Dating/styles/styles.css?<?php echo time(); ?>">
+    <link rel="stylesheet"
+          href="http://jbose.greenriverdev.com/IT328/Bose_Jarod_Dating/styles/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <figure id="border">
+        <figcaption>
+            <h1 class="ml-5"><br><strong>My Dating Website</strong></h1>
+            <p id="summary">Welcome to the web's most successful dating website. <strong>At My Dating</strong>
+                Website you'll meet another like minded individuals. We have the
+                highest success rate of couples on the web. User's are matched by
+                interest and location. Find out why so many others have found love on
+                our site<br><br>
+            </p>
+            <h4 class="ml-5"><strong>Hear what our users are saying about us.</strong></h4>
+            <p id="review">"I met the love of my life after only a month" - Andrea<br><br>
+                "It was so easy to set up and profile and start meeting people.
+                I didn't realize how many others were looking for love in my
+                area." -John Smith<br><br>
+                "Just try it! You'll never be the same!" - Sarah</p><br><br>
+            <button class="btn btn-primary ml-5">Create a Profile!</button>
+        </figcaption>
+        <img id="datePic" class="img-fluid rounded"
+             src="http://jbose.greenriverdev.com/IT328/Bose_Jarod_Dating/images/best-dating-website.jpg"
+             alt="Dating Picture">
+    </figure>
+</div>
+<script src="js/jquery.slim.min.js"></script>
+<script src="js/tether.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ pages/Profile_Summary.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: humme_000
+ * Date: 2/2/2018
+ * Time: 11:52 AM
+ */
